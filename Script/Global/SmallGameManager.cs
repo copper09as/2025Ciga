06cda803fd8158b1009393e4f213bcf47c5f19ba
copper <494699676@qq.com>
2025-06-28 23:25:06 +0800

    优化测试逻辑

--- Script/Global/SmallGameManager.cs
+++ Script/Global/SmallGameManager.cs
@@ -5,6 +5,10 @@ public partial class SmallGameManager : Node
 {
     public static SmallGameManager Instance { get; private set; }
     public bool isStartSmallGame;
+    [Export]
+    public Vector2 game1;
+    [Export]
+    public Vector2 game2;
     public Npc currentGameNpc;
     public override void _Ready()
     {
@@ -13,6 +17,8 @@ public partial class SmallGameManager : Node
         {
             Instance = this;
         }
+        else
+            QueueFree();
     }
     public void CreateSmallGame(GameType gameType, Npc npc)
     {
@@ -22,7 +28,7 @@ public partial class SmallGameManager : Node
         {
             case GameType.Beat:
                 var beat_game = ResManager.Instance.CreateInstance<BeatGame>(StringResource.BeatGame, this);
-                beat_game.Position = new Vector2(600,100);
+                beat_game.Position = game1;
                 break;
             case GameType.Listen: break;
         }
